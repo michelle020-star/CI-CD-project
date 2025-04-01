b9b8ec08f5aa2356976ac5d9a0a4662d35e8346c
michelle020-star <michellesilvano020@gmail.com>
2025-04-02 00:19:49 +0800

    Updated script.py

--- script.py
+++ script.py
@@ -2,4 +2,4 @@ def add(a, b):
     return a + b
 
 if __name__== "__main__":
-    print(f"2 + 3 = {add(2, 3)}")
+    print(f"5 + 3 = {add(5, 3)}")
--- test_script.py
+++ test_script.py
@@ -1,4 +1,4 @@
 import script
 
 def test_add():
-    assert script.add(2, 3) == 5
+    assert script.add(5, 3) == 8
